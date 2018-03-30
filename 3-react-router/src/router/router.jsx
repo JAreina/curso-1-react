12b416a83894ab3b63e9cd11899a2d63608c3181
JAreina <jareinafdez@gmail.com>
2018-03-30 19:38:21 +0200

    yarn add react-router-dom

---
+++ 3-react-router/src/router/router.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Redirect,
+    withRouter
+} from 'react-router-dom';
+
+
+
+const Navega= ()=>(
+    <h1> REACT ROUTER </h1>
+)
+
+export default Navega;
