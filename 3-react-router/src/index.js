7dc354ec0a0704e21842a14e6b98200bbee77051
JAreina <jareinafdez@gmail.com>
2018-03-30 19:41:05 +0200

    router

--- 3-react-router/src/index.js
+++ 3-react-router/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './componentes/';
 
+import Navega from './router/router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Navega />, document.getElementById('root'));
