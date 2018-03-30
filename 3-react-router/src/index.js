82fa8b1f3a0c4f0a2d21cffa55399d0d109ce9b7
JAreina <jareinafdez@gmail.com>
2018-03-30 19:56:30 +0200

    router

--- 3-react-router/src/index.js
+++ 3-react-router/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './componentes/';
+import css from './index.css';
+//import App from './componentes/';
 
 import Navega from './router/router';
 
