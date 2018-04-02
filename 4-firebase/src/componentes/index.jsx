373bee57db8ad982e6dfef8228f7f0f8a83dae57
JAreina <jareinafdez@gmail.com>
2018-04-02 20:23:50 +0200

    menu con purecss

--- 4-firebase/src/componentes/index.jsx
+++ 4-firebase/src/componentes/index.jsx
@@ -14,9 +14,20 @@ import 'pure-css';
 import './index.css';
 
 import $ from 'jquery';
+// firebase 
 import {firebaseAuth} from './data/config'
 
-// componente con estado
+import logo from './media/jareina.png'
+
+// componentes pages
+import Home from './pages/index';
+import About from './pages/about';
+import Error404 from './pages/error404';
+import Login from './pages/login';
+import Registro from './pages/registro';
+import Cursos from './pages/protegidas/index';
+
+
 
 class App extends Component {
     constructor(...props) {
@@ -28,6 +39,7 @@ class App extends Component {
             prueba: 'hola'
         }
         this.metodoPrueba = this.metodoPrueba.bind(this);
+        this.clickMenu = this.clickMenu.bind(this);
     }
 
     metodoPrueba() {
@@ -41,30 +53,56 @@ class App extends Component {
         return (
             this.state.cagando === true
                 ? <h4>Cagando</h4>
-                : (   <div>
+                : (   <div >
                     <Router>
-                    <div>
+                    <div className="">
                             <header className="custom-menu-wrapper">
                                 <div className="pure-menu custom-menu custom-menu-top">
-                                    <a href="#" className="pure-menu-heading custom-menu-brand">Brand</a>
-                                    <a href="#" className="custom-menu-toggle" id="toggle"><s className="bar"></s><s className="bar"></s></a>
+                                    <a href="#" className="pure-menu-heading custom-menu-brand">
+                                    <div className="pure-g">
+                                    <img  className="pure-u-24-24"src={logo} alt="logo"/>
+                                    </div>
+                                    </a>
+                                    <a href="#" className="custom-menu-toggle" 
+                                    onClick={this.clickMenu}
+                                    id="toggle">
+                                    
+                                    <s className="bar"></s>
+                                    <s className="bar"></s>
+                                   
+                                    
+                                    </a>
                                 </div>
                                 <div className="pure-menu pure-menu-horizontal pure-menu-scrollable custom-menu custom-menu-bottom custom-menu-tucked" id="tuckedMenu">
                                     <div className="custom-menu-screen"></div>
                                     <ul className="pure-menu-list">
-                                        <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-                                        <li className="pure-menu-item"><a href="#" className="pure-menu-link">About</a></li>
-                                        <li className="pure-menu-item"><a href="#" className="pure-menu-link">Contact</a></li>
-                                        <li className="pure-menu-item"><a href="#" className="pure-menu-link">Blog</a></li>
-                                        <li className="pure-menu-item"><a href="#" className="pure-menu-link">GitHub</a></li>
-                                       
+                                        <li className="pure-menu-item" >
+                                        <Link to='/' className="pure-menu-link" onClick={this.clickMenu}>Inicio</Link>
+                                            
+                                        </li>
+                                        <li className="pure-menu-item" >
+                                        <Link to='/about' className="pure-menu-link" onClick={this.clickMenu}>About</Link>
+                                            
+                                        </li>
+                                        <li className="pure-menu-item" >
+                                        <Link to='/registro' className="pure-menu-link" onClick={this.clickMenu}>Registro</Link>
+                                            
+                                        </li>
+                                        <li className="pure-menu-item" >
+                                        <Link to='/login' className="pure-menu-link" onClick={this.clickMenu}>Login</Link>
+                                            
+                                        </li>
                                     </ul>
                                 </div>
                             
                             </header>
                        <main className="main">
-                             <Switch>
-
+                             <Switch>  
+                             <Route path="/"  exact component={Home}/>
+     <Route path="/about" component={About}/>
+     <Route path="/login" component={Login}/>
+     <Route path="/registro" component={Registro}/>
+     <Route path="/error" component={Error404}/>
                                  </Switch>
                        </main>
                        
@@ -77,16 +115,15 @@ class App extends Component {
                         </div>*/
                 )
           }
+
+          clickMenu(){
+                 
+                  document.getElementById('tuckedMenu').classList.toggle('custom-menu-tucked');
+                  document.getElementById('toggle').classList.toggle('x');
+          }
 }
 App.propTypes = {};
 App.defaultProps={};
   
-/*
-(function (window, document) {
-    document.getElementById('toggle').addEventListener('click', function (e) {
-        document.getElementById('tuckedMenu').classList.toggle('custom-menu-tucked');
-        document.getElementById('toggle').classList.toggle('x');
-    });
-    })(this, this.document);
-  */                      
+                  
 export default App;
---
+++ 4-firebase/src/componentes/pages/index.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Home extends Component{
+    render(){
+        return (
+            <div className="Main-container">
+               <div>PÁGINA home</div>
+            </div>
+        )
+    }
+}
