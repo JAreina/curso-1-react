913aac9a83dc33d592766f10c396b223d34c640c
JAreina <jareinafdez@gmail.com>
2018-04-03 19:24:53 +0200

    formulario login

--- 5-app-react-juan/src/componentes/index.jsx
+++ 5-app-react-juan/src/componentes/index.jsx
@@ -15,7 +15,7 @@ import './index.css';
 
 import $ from 'jquery';
 // firebase 
-import {firebaseAuth} from './data/config'
+import { firebaseAuth } from './data/config'
 
 import logo from './media/jareina.png'
 
@@ -26,6 +26,36 @@ import Error404 from './pages/error404';
 import Login from './pages/login';
 import Registro from './pages/registro';
 import Cursos from './pages/protegidas/index';
+import { logout } from './helpers/auth'
+
+
+const PrivateRoute = ({ component: Component, autenticado, rest }) => (
+    <Route
+        {...rest}
+        render={
+            props => autenticado === true
+                ? <Component {...props} />
+                : <Redirect to={
+                    {
+                        pathname: '/login',
+                        state: { from: props.location }
+                    }
+                } />
+        }
+    />
+);
+
+const PublicRoute = ({ component: Component, autenticado, rest }) => (
+    <Route
+        {...rest}
+        render={
+            props => autenticado === false
+                ? <Component {...props} />
+                : <Redirect to='/cursos'  />
+        }
+    />
+);
+
 
 
 
@@ -53,79 +83,124 @@ class App extends Component {
         return (
             this.state.cagando === true
                 ? <h4>Cagando</h4>
-                : (   <div >
+                : (<div >
                     <Router>
-                    <div className="">
+                        <div className="">
                             <header className="custom-menu-wrapper">
                                 <div className="pure-menu custom-menu custom-menu-top">
-                                    <a  className="pure-menu-heading custom-menu-brand">
-                                    <div className="pure-g">
-                                    <img  className="pure-u-12-12"src={logo} alt="logo"/>
-                                    </div>
+                                    <a className="pure-menu-heading custom-menu-brand">
+                                        <div className="pure-g">
+                                            <img className="pure-u-12-12" src={logo} alt="logo" />
+                                        </div>
                                     </a>
-                                    <a  className="custom-menu-toggle" 
-                                    onClick={this.clickMenu}
-                                    id="toggle">
-                                    
-                                    <s className="bar"></s>
-                                    <s className="bar"></s>
-                                   
-                                    
+                                    <a className="custom-menu-toggle"
+                                        onClick={this.clickMenu}
+                                        id="toggle">
+
+                                        <s className="bar"></s>
+                                        <s className="bar"></s>
+
+
                                     </a>
                                 </div>
                                 <div className="pure-menu pure-menu-horizontal pure-menu-scrollable custom-menu custom-menu-bottom custom-menu-tucked" id="tuckedMenu">
                                     <div className="custom-menu-screen"></div>
                                     <ul className="pure-menu-list">
                                         <li className="pure-menu-item" >
-                                        <Link to='/' className="pure-menu-link" onClick={this.clickMenu}>Inicio</Link>
-                                            
+                                            <Link to='/' className="pure-menu-link" onClick={this.clickMenu}>Inicio</Link>
+
                                         </li>
                                         <li className="pure-menu-item" >
-                                        <Link to='/about' className="pure-menu-link" onClick={this.clickMenu}>About</Link>
-                                            
+                                            <Link to='/about' className="pure-menu-link" onClick={this.clickMenu}>About</Link>
+
                                         </li>
-                                        <li className="pure-menu-item" >
-                                        <Link to='/registro' className="pure-menu-link" onClick={this.clickMenu}>Registro</Link>
-                                            
-                                        </li>
-                                        <li className="pure-menu-item" >
-                                        <Link to='/login' className="pure-menu-link" onClick={this.clickMenu}>Login</Link>
-                                            
-                                        </li>
-                                        
+
+
+
+                                        {
+                                            (this.state.autenticado)
+                                                ? <span>
+                                                    <li className="pure-menu-item" >
+                                                        <Link to='/cursos' className="pure-menu-link" onClick={this.clickMenu}>Cursos</Link>
+
+                                                    </li>
+                                                    <li className="pure-menu-item" >
+                                                        <Link to='/logout' className="pure-menu-link"
+                                                            onClick={() => {
+                                                                logout()
+                                                                this.setState({ autenticado: false })
+                                                                this.clickMenu()
+                                                            }
+                                                            }>Logout</Link>
+
+                                                    </li>
+                                                </span>
+                                                : <span>
+                                                    <li className="pure-menu-item" >
+                                                        <Link to='/registro' className="pure-menu-link" onClick={this.clickMenu}>Registro</Link>
+
+                                                    </li>
+                                                    <li className="pure-menu-item" >
+                                                        <Link to='/login' className="pure-menu-link" onClick={this.clickMenu}>Login</Link>
+
+                                                    </li>
+                                                </span>
+                                        }
+
                                     </ul>
                                 </div>
-                            
-                            </header>
-                       <main className="main">
-                             <Switch>  
-                             <Route path="/"  exact component={Home}/>
-     <Route path="/about" component={About}/>
-     <Route path="/login" component={Login}/>
-     <Route path="/registro" component={Registro}/>
-     <Route path="/error" component={Error404}/>
-  
-                                 </Switch>
-                       </main>
-                       
-                       </div>
-                       </Router> </div>
-                        )
-                 /*   <div>
-                            <p>{this.state.prueba}</p>
-                            <button onClick={this.metodoPrueba}>click</button>
-                        </div>*/
-                )
-          }
 
-          clickMenu(){
-                 
-                  document.getElementById('tuckedMenu').classList.toggle('custom-menu-tucked');
-                  document.getElementById('toggle').classList.toggle('x');
+                            </header>
+                            <main className="main">
+                                <Switch>
+                                    <Route path="/" exact component={Home} />
+                                    <Route  path="/about" component={About} />
+                                    <PublicRoute autenticado={this.state.autenticado} path="/login" component={Login} />
+                                    <PublicRoute autenticado={this.state.autenticado} path="/registro" component={Registro} />
+                                    <Route path="/error" component={Error404} />
+                                    <PrivateRoute autenticado={this.state.autenticado}  path="/cursos" component={Cursos} />
+                                </Switch>
+                            </main>
+
+                        </div>
+                    </Router> </div>
+                )
+            /*   <div>
+                       <p>{this.state.prueba}</p>
+                       <button onClick={this.metodoPrueba}>click</button>
+                   </div>*/
+        )
+    }
+
+    clickMenu() {
+
+        document.getElementById('tuckedMenu').classList.toggle('custom-menu-tucked');
+        document.getElementById('toggle').classList.toggle('x');
+    }
+
+    componentDidMount() {
+        this.removeListener = firebaseAuth().onAuthStateChanged( user => {
+          if (user) {
+            this.setState({
+              autenticado: true,
+              cagando: false
+            })
+          } else {
+            this.setState({
+              cagando: false
+            })
           }
+        })
+      }
+    
+      componentWillUnmount() {
+        this.removeListener()
+      }
+
+
 }
 App.propTypes = {};
-App.defaultProps={};
-  
-                  
+App.defaultProps = {};
+
+
 export default App;
--- 5-app-react-juan/src/componentes/pages/login.jsx
+++ 5-app-react-juan/src/componentes/pages/login.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import {login} from '../helpers/auth';
-
+import {login,resetPassword} from '../helpers/auth';
+import 'pure-css/lib/buttons.css';
+import 'pure-css/lib/forms.css';
+import './login.css'
 export default class Login extends Component{
 
    constructor(...props){
@@ -8,37 +10,49 @@ export default class Login extends Component{
 
        this.state = {loginMensaje: null};
        this.enviar= this.enviar.bind(this);
+       this.reseteaPassword= this.reseteaPassword.bind(this);
    }
 
+
+   reseteaPassword(){
+       resetPassword(this.email.value)
+        .then(()=>this.setState(this.setMensaje(`SE HA ENVIADO UN CORREO 
+        PARA RESTABLECER LA CONTRASEÑA A ${this.email.value}`)))
+        .catch(err=>this.setMensaje(this.setMensaje(`El email ${this.email.value} no está registrado`)))
+   }
      enviar(e){
          e.preventDefault();
          alert("enviadndo formularios")
          login(this.email.value,this.password.value)
-              .catch(error => this.setState(this.setErrorMensaje("USUARIO ERRÓNEO")));
+              .catch(error => this.setState(this.setMensaje("USUARIO ERRÓNEO")))
      }
 
 
-     setErrorMensaje(err){
-         return {loginMensaje:err}
+     setMensaje(msg){
+         return {loginMensaje:msg}
      }
 
     render(){
         return (
             <div className="Main-container">
-               <div>PÁGINA login</div>
-               <form onSubmit={this.enviar}>
+               <h1 >Login </h1>
+               
+               <form onSubmit={this.enviar} className="AuthForm">
+              
                <input type="email" placeholder="tu email"
                 ref={email=>this.email = email}/>
                <input type="password" placeholder="tu nombre de usuario"
                ref={pass=>this.password = pass}/>
 
+               
+               <input type="submit" value="loguearse" className="pure-button pure-button-primary"/>
                {
                    this.state.loginMensaje && 
-                   <div className="error">
-                       Error : {this.state.loginMensaje}
+                   <div className="u-error">
+                        {this.state.loginMensaje}
+                       <a href="#" className="alert-link" onClick={this.reseteaPassword()}>    Recuperar la contraseña </a>
                    </div>
                }
-               <input type="submit" value="loguearse"/>
                </form>
             </div>
         )
